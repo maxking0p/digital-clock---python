77f957e94b538059c48b48a1928b18b5919cbba7
maxking0p <88237637+maxking0p@users.noreply.github.com>
2021-10-14 10:43:01 +0530

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+
+root = Tk()
+
+root.title("clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+label = Label(root,font=("ds-digital",80), background = "black", foreground = "red")
+label.pack(anchor='center')
+time()
+
+mainloop()
+
+'''don't forget to follow me on instagram @maxkingop'''
+
+
+
+
+
+
+
